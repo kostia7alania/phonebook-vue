d09c951d28f2aa01dd018bd821453f33b72e0719
kostia bazrow <osetia-alania@mail.ru>
2019-07-03 20:23:56 +0300

    footer

---
+++ src/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+      <v-footer absolute class="">
+        <v-layout row wrap align-center>
+          <v-flex xs12>
+            <div class=" ml-3">
+              Test task
+              <v-icon class="red--text">favorite</v-icon>
+              by <a class="" href="https://github.com/kostia7alania" target="_blank">@kostia7alania</a>
+              &copy; {{ new Date().getFullYear() }}
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-footer>
+</template>
