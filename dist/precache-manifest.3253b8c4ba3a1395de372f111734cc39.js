c5b676f3c9d78b895263a7735c1dc38d40ea95a2
kostia bazrow <osetia-alania@mail.ru>
2019-07-03 15:36:36 +0300

    build for git pages

---
+++ dist/precache-manifest.3253b8c4ba3a1395de372f111734cc39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47840730fc04ed162f18",
+    "url": "/css/app.8448cf90.css"
+  },
+  {
+    "revision": "47840730fc04ed162f18",
+    "url": "/js/app.f609d33e.js"
+  },
+  {
+    "revision": "48dd4780cee20f62ee79",
+    "url": "/css/chunk-vendors.a331ea65.css"
+  },
+  {
+    "revision": "48dd4780cee20f62ee79",
+    "url": "/js/chunk-vendors.ce122e28.js"
+  },
+  {
+    "revision": "50f1a0e644362fb9a0fed0d2b9e493aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
