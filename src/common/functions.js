9668bb41e8d78de77949e0f9e4acddb0fb8b704d
kostia bazrow <osetia-alania@mail.ru>
2019-07-03 16:11:07 +0300

    fix compare function

--- src/common/functions.js
+++ src/common/functions.js
@@ -1,7 +1,12 @@
 export const getGuid = () => Math.floor(Math.random()*100000000000)
 export const compare = (a, b, key = 'name') => {
-    if (a[key]&&a[key].toUpperCase() < b[key]&&b[key].toUpperCase()) return -1;
-    if (a[key]&&a[key].toUpperCase() > b[key]&&b[key].toUpperCase()) return 1;
+    if(typeof a[key] == 'string') {
+        if (a[key]&&a[key].toUpperCase() < b[key]&&b[key].toUpperCase()) return -1;
+        if (a[key]&&a[key].toUpperCase() > b[key]&&b[key].toUpperCase()) return 1;
+    } else {
+        if (a[key] < b[key]) return -1;
+        if (a[key] > b[key]) return 1;
+    }
     return 0;
   }
  
