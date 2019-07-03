673fcbb93e0f89d6db3d711ac6ef5ba62bc1e8f1
kostia bazrow <osetia-alania@mail.ru>
2019-07-03 20:24:44 +0300

    search issues fix

--- src/components/SearchList.vue
+++ src/components/SearchList.vue
@@ -1,31 +1,26 @@
 <template>
- <v-autocomplete
-        :items="items"
-        v-model="selected"
-        item-text="name"
-        item-value="name"
-        autofocus
-        clearable
-        solo
-        dark
-        hint="type..."
-        label="I'm looking for..."
-        persistent-hint 
-      >
-        <template v-slot:append-outer>
-          <v-slide-x-reverse-transition
-            mode="out-in"
-          > 
-          </v-slide-x-reverse-transition>
-        </template>
-      </v-autocomplete>
- 
+  <v-autocomplete
+    :items="items"
+    v-model="selected"
+    item-text="name"
+    item-value="name"
+    autofocus
+    clearable
+    solo
+    dark
+    hint="type..."
+    label="I'm looking for..."
+    persistent-hint 
+  >
+  <template v-slot:append-outer>
+    <v-slide-x-reverse-transition mode="out-in"></v-slide-x-reverse-transition>
+  </template>
+  </v-autocomplete>
 </template>
 
 
 <script>
-import { mapGetters, mapActions, mapMutations } from 'vuex'
-
+import { mapGetters, mapMutations } from 'vuex'
   export default {
     data () {
       return { 
@@ -33,19 +28,31 @@ import { mapGetters, mapActions, mapMutations } from 'vuex'
       }
     }, 
     watch: {
+      COUNT_FILTERED_ITEMS(count) {
+        if(count == 0) {
+          this.SET_FILTER_BY_NAME(null)
+          this.SET_IS_SEARCH_MODE(false)
+
+        }
+      },
       selected(neww) {
         this.SET_FILTER_BY_NAME(neww)
       },
       items(newItems) {
+        if(!this.FILTER_ITEMS_BY_NAME) return;
         if(!newItems.filter(item => item.name == this.FILTER_ITEMS_BY_NAME).length) {
           this.selected = null
           this.SET_IS_SEARCH_MODE(false)
         }
       }
     },
+    beforeDestroy(){
+      this.SET_FILTER_BY_NAME(null)
+      this.SET_IS_SEARCH_MODE(false)
+    },
     computed: {
       ...mapGetters([
-        'items', 'FILTER_ITEMS_BY_NAME'
+        'items', 'FILTER_ITEMS_BY_NAME', 'COUNT_FILTERED_ITEMS'
       ])
     },
     methods: {
